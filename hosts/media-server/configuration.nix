9ceb2688f20fee7cf62dc47c0795ef8a1c2274b4
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2025-05-19 00:14:41 +0700

    fix: puid and pgid

--- hosts/media-server/configuration.nix
+++ hosts/media-server/configuration.nix
@@ -68,8 +68,8 @@
         "/media:/library"
       ];
       environment = {
-        PUID = toString config.users.users.media.uid;
-        PGID = toString config.users.groups.media.gid;
+        PUID = "1000";
+        PGID = "1000";
       };
       ports = [
         "127.0.0.1:8889:8888"
