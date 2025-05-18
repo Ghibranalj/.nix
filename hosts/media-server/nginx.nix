a045008afd7ae33e0c230f0188f09d96349c7f33
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2025-05-19 03:36:06 +0700

    fix: nginx for transmission

--- hosts/media-server/nginx.nix
+++ hosts/media-server/nginx.nix
@@ -82,6 +82,12 @@
           proxyPass =
             "http://127.0.0.1:9091"; # Default transmission web UI port
           proxyWebsockets = true;
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
         };
       };
 
