f850a9ad7596f9bc8ba05c609ea15e9c6bca6b57
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2025-05-19 03:24:42 +0700

    fix: change doom font size for tuf

--- hosts/tuf/home.nix
+++ hosts/tuf/home.nix
@@ -4,6 +4,9 @@
 
   gtk-theme.enable = true;
   doom.mode = "gui";
+
+  doom.fontSize.normal = "15";
+  doom.fontSize.big = "18";
   home.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS =
       "\\\${HOME}/.steam/root/compatibilitytools.d";
