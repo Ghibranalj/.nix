b3500e7119c74c111031ce3a1d9100b9b9c4cf02
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2025-06-13 06:42:47 +0700

    feat: add untested sddm theme

--- sys/lightdm.nix
+++ sys/lightdm.nix
@@ -8,6 +8,30 @@
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      theme = "materia-dark";
+      package = pkgs.sddm.override {
+        themes = [
+          (pkgs.stdenv.mkDerivation {
+            name = "sddm-materia-dark";
+            src = pkgs.fetchFromGitHub {
+              owner = "PapirusDevelopmentTeam";
+              repo = "materia-kde";
+              rev = "6cc4c1867c78b62f01254f6e369ee71dce167a15";
+              sha256 = "sha256-tZWEVq2VYIvsQyFyMp7VVU1INbO7qikpQs4mYwghAVM=";
+            };
+            installPhase = ''
+              mkdir -p $out/share/sddm/themes
+              cp -r sddm/themes/materia-dark $out/share/sddm/themes/
+
+              # Replace the background image with custom one
+              rm $out/share/sddm/themes/materia-dark/images/background.png
+              cp ${
+                ./files/background.png
+              } $out/share/sddm/themes/materia-dark/images/background.png
+            '';
+          })
+        ];
+      };
     };
     programs.hyprland.enable = true;
   };
